c3e8d5a161d74d58af9add888bd312d3da5dc3c6
Takashi AOKI <federkasten@me.com>
2014-02-27 19:04:59 +0900

    fix clojurescript version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
   :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2073"]]
+                 [org.clojure/clojurescript "0.0-2173"]]
   :plugins [[lein-cljsbuild "1.0.2"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds {:useless
