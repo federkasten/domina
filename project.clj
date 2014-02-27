d7e6152acf2575ee657033780a70adbbab0e3e9d
Takashi AOKI <federkasten@me.com>
2014-02-27 19:00:20 +0900

    bump up dependences versions

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
   :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2030"]]
-  :plugins [[lein-cljsbuild "1.0.0"]]
+                 [org.clojure/clojurescript "0.0-2073"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds {:useless
                        {:source-paths ["src/cljs"]}}})
